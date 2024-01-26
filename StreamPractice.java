bbe352e8ebf9658db6b85c6bfb9e81e9a9b56e63
acm <azeemuk80@gmail.com>
2024-01-25 22:23:43 -0500

    Updated Duplicates approach 2

--- StreamPractice.java
+++ StreamPractice.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamPractice {
-
     public static void main(String[] args){
         List<Integer> list = IntStream.of(0,1,2,2,3,3,3,4,5).boxed().toList();
         List<Integer> resultantDuplicates=list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
