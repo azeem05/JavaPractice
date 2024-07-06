d7586a25258fab3fbc49b3ba548db8221751549a
acm <azeemuk80@gmail.com>
2024-07-06 11:31:58 -0400

    Added Terminal operation program

---
+++ TerminalOperationStream.java
@@ -0,0 +1,21 @@
+
+import java.util.Arrays;
+
+public class TerminalOperationStream {
+    public static void main(String[] args) {
+        System.out.println("Stream without terminal operation");
+
+        Arrays.stream(new int[] { 1, 2, 3 }).map(i -> {
+            System.out.println("doubling " + i);
+            return i * 2;
+        });
+
+        System.out.println("Stream with terminal operation");
+        int sum=Arrays.stream(new int[] { 1, 2, 3 }).map(i -> {
+            System.out.println("doubling " + i);
+            return i * 2;
+        }).sum();
+        System.out.println(sum);
+        //Terminal operations include forEach, reduce, collect and sum.
+    }
+}
