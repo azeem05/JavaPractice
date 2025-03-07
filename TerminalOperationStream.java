6727c3b1e07ee2eeec46b012d7507b3962b4e6e1
azeem05 <68479378+azeem05@users.noreply.github.com>
2025-03-06 19:25:58 -0500

    Add files via upload
    Stream Terminal operation

--- TerminalOperationStream.java
+++ TerminalOperationStream.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 
 public class TerminalOperationStream {
@@ -6,13 +5,13 @@ public class TerminalOperationStream {
         System.out.println("Stream without terminal operation");
 
         Arrays.stream(new int[] { 1, 2, 3 }).map(i -> {
-            System.out.println("doubling " + i);
+            System.out.println("non-terminal doubling " + i);//this wont be printed as no terminal operation is available
             return i * 2;
         });
 
         System.out.println("Stream with terminal operation");
         int sum=Arrays.stream(new int[] { 1, 2, 3 }).map(i -> {
-            System.out.println("doubling " + i);
+            System.out.println("terminal: doubling " + i);
             return i * 2;
         }).sum();
         System.out.println(sum);
